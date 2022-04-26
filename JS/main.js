1253cd363383482b385f10276940bc42952bf7d3
SU-T <guitaristpeet@gmail.com>
2022-04-26 14:35:52 -0700

    nav-header for small screen

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
-
+const navHeader = document.querySelector('.nav-header');
+let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('openMenu');
+    if (!menuOpen) {
+        menuBtn.classList.add('openMenu');
+        navHeader.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('openMenu');
+        navHeader.classList.remove('open');
+        menuOpen = false;
+    }
 });
+// menuBtn.classList.toggle('openMenu');
